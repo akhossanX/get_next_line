f482a3a86d3481d605f9eae8b46425bd165c23f2
Abdelilah Khossan <akhossan@e1r10p12.1337.ma>
2019-04-13 23:09:41 +0200

    deleting exec file

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: akhossan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/07 14:11:44 by akhossan          #+#    #+#             */
-/*   Updated: 2019/04/12 22:53:02 by akhossan         ###   ########.fr       */
+/*   Updated: 2019/04/13 23:08:30 by akhossan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,16 +32,21 @@ int		get_next_line(const int fd, char **line)
 			return (-1);
 	}
 	ft_bzero(buff, BUFF_SIZE + 1);
+	ft_bzero(*line, BUFF_SIZE + 1);
 	while ((bytes = read(fd, buff, BUFF_SIZE)) > 0)
 	{
-		printf("buff inside gnl : %s\n", buff);
 		if (check_line(buff, line, &static_line) == 1)
+		{
+			printf("byte: %d\n", bytes);
+			printf("buff: %s\n", buff);
+			printf("over: %s\n", static_line);
 			return (1);
-		tmp = static_line;
-		static_line = ft_strjoin(static_line, buff);
-		printf("overflow buffer : %s\n", static_line);
+		}
+		tmp = *line;
+		*line = ft_strjoin(*line, buff);
 		free(tmp);
 	}
+	//printf("line inside gnl: %s\n", *line);
 	return (bytes < 0 ? -1: 0);
 }
 
@@ -53,19 +58,24 @@ int		check_line(char *buff, char **line, char **static_line)
 	/* A line has been read */
 	if ((endl = ft_strchr(buff, '\n')) != NULL)
 	{
-
 		/* is '\n' found before the end of buff? */
 		/* if so save the overflow to static line */ 
-		if (endl < buff + ft_strlen(buff) - 1)
-		{
-			tmp = *static_line;
-			*static_line = ft_strdup(endl + 1);
-			free(tmp);
-		}
+		//if (endl < buff + ft_strlen(buff) - 1)
+		//{
+		//	endl[0] = '\0';
+		//	tmp = *static_line;
+		//	*static_line = ft_strdup(endl + 1);
+		//	free(tmp);
+		//}
 		/* copy the line string into *line */
 		endl[0] = '\0';
 		tmp = *line;
-		*line = ft_strdup(buff);
+		*line = ft_strjoin(*line, buff);
+		/* copy the overflow to static_lin */
+		tmp = *static_line;
+		*static_line = ft_strdup(endl + 1);
+		//printf("line inside chk: %s\n", *line);
+		//printf("buff inside chk: %s\n", buff);
 		free(tmp);
 		return (1);
 	}
@@ -83,8 +93,6 @@ int		main(int ac, char **av)
 	if ((fd = open(av[1], O_RDONLY)) < 0)
 		exit(1);
 	if ((flag = get_next_line(fd, &line)) > 0)
-	{
-		printf("Successful reading performed\n");
-		printf("%s", line);
-	}
+		printf("line: %s\n", line);
+	close(fd);
 }
