5231dce828d2a880e5c8e3fd1e5b4b7579ddfd85
Abdelilah Khossan <akhossan@e2r1p3.1337.ma>
2019-04-16 17:17:37 +0100

    test files added

--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: akhossan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/06 22:04:56 by akhossan          #+#    #+#             */
-/*   Updated: 2019/04/12 21:20:47 by akhossan         ###   ########.fr       */
+/*   Updated: 2019/04/16 14:49:01 by akhossan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE	10
+# define BUFF_SIZE	2
 # include <unistd.h>
 # include <stdlib.h>
 # include "libft.h"
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: akhossan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/07 14:11:44 by akhossan          #+#    #+#             */
-/*   Updated: 2019/04/13 23:08:30 by akhossan         ###   ########.fr       */
+/*   Updated: 2019/04/16 15:01:20 by akhossan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include <string.h>
 #include "get_next_line.h"
 #include <stdlib.h>
+#include <stdio.h>
 #include <fcntl.h>
 
 int		check_line(char *buff, char **line, char **static_line);
@@ -21,33 +22,35 @@ int		check_line(char *buff, char **line, char **static_line);
 int		get_next_line(const int fd, char **line)
 {
 	char		buff[BUFF_SIZE + 1];
-	static char	*static_line;
+	static char	*static_line = NULL;
 	int			bytes;
 	char		*tmp;
 
-	static_line = (char *)malloc(0);
-	if (!line || !*line)
-	{
-		if(!(*line = (char *)malloc(sizeof(char) * (BUFF_SIZE + 1))))
+	if (!static_line)
+		static_line = (char *)malloc(0);
+	//if (!*line)
+		//if(!(*line = (char *)malloc(sizeof(char) * (BUFF_SIZE + 1))))
+	if (!(*line = ft_strdup(static_line)))
 			return (-1);
-	}
 	ft_bzero(buff, BUFF_SIZE + 1);
-	ft_bzero(*line, BUFF_SIZE + 1);
 	while ((bytes = read(fd, buff, BUFF_SIZE)) > 0)
 	{
 		if (check_line(buff, line, &static_line) == 1)
-		{
-			printf("byte: %d\n", bytes);
-			printf("buff: %s\n", buff);
-			printf("over: %s\n", static_line);
 			return (1);
-		}
 		tmp = *line;
 		*line = ft_strjoin(*line, buff);
 		free(tmp);
 	}
-	//printf("line inside gnl: %s\n", *line);
-	return (bytes < 0 ? -1: 0);
+	if (bytes == 0)
+	{
+		printf("end buff:[%s]", buff);
+		printf("end line:[%s]", *line);
+		tmp = *line;
+		*line = ft_strjoin(*line, buff);
+		free(tmp);
+		return (0);
+	}
+	return (-1);
 }
 
 int		check_line(char *buff, char **line, char **static_line)
@@ -56,7 +59,7 @@ int		check_line(char *buff, char **line, char **static_line)
 	char	*endl;
 
 	/* A line has been read */
-	if ((endl = ft_strchr(buff, '\n')) != NULL)
+	if ((endl = ft_strchr(buff, '\n')))
 	{
 		/* is '\n' found before the end of buff? */
 		/* if so save the overflow to static line */ 
@@ -74,6 +77,7 @@ int		check_line(char *buff, char **line, char **static_line)
 		/* copy the overflow to static_lin */
 		tmp = *static_line;
 		*static_line = ft_strdup(endl + 1);
+		//printf("over: [%s]", *static_line);
 		//printf("line inside chk: %s\n", *line);
 		//printf("buff inside chk: %s\n", buff);
 		free(tmp);
@@ -87,12 +91,18 @@ int		main(int ac, char **av)
 	int		fd;
 	char	*line;
 	int		flag;
+	int		i = 1;
 
 	if (ac != 2)
 		exit(1);
+	line = NULL;
 	if ((fd = open(av[1], O_RDONLY)) < 0)
 		exit(1);
-	if ((flag = get_next_line(fd, &line)) > 0)
-		printf("line: %s\n", line);
+	flag = 1;
+	while (flag > 0)
+	{
+		flag = get_next_line(fd, &line);
+		printf("line%d:[%s]", i++, line);
+	}
 	close(fd);
 }
