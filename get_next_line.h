5e1a4b6c9a3ae5dbdecb554c2213b31cc668a2f5
Abdelilah Khossan <akhossan@e2r4p15.1337.ma>
2019-04-18 22:47:40 +0100

    supporting multiple fds

--- get_next_line.h
+++ get_next_line.h
@@ -6,22 +6,22 @@
 /*   By: akhossan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/06 22:04:56 by akhossan          #+#    #+#             */
-/*   Updated: 2019/04/18 21:22:29 by akhossan         ###   ########.fr       */
+/*   Updated: 2019/04/18 22:45:26 by akhossan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # define BUFF_SIZE	999
+# define FILED_MAX	4864
 # include <unistd.h>
 # include <stdlib.h>
 # include "libft.h"
 
 typedef struct		s_filed
 {
-	int				fd;
-	char			*overflow;
-	struct s_filed	*next;
+	int				fd_arr[FILED_MAX];
+	char			**overflow;
 }					t_filed;
 
 int					get_next_line(int fd, char **line);
