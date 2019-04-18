8b007bb908deb113c4a73490caf35dfd1a1239be
Abdelilah Khossan <akhossan@e2r4p15.1337.ma>
2019-04-18 21:54:37 +0100

    structuring code in accordance to norm

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: akhossan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/06 22:03:34 by akhossan          #+#    #+#             */
-/*   Updated: 2019/04/17 22:42:42 by abkh             ###   ########.fr       */
+/*   Updated: 2019/04/18 21:12:40 by akhossan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,14 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
-#define	ALLOC_LINE(line)		if(!(line = ft_strnew(0))) return (-1)
-#define	ALLOC_OVERFLOW(over)	if(!(over = ft_strnew(BUFF_SIZE))) return (-1)
+#define ALLOC_LINE(line)		if(!(line = ft_strnew(0))) return (-1)
+#define ALLOC_OVERFLOW(over)	if(!(over = ft_strnew(BUFF_SIZE))) return (-1)
 
 /*
 ** Joins s2 to s1 and frees the old s1
 */
-void	ft_strjoinfree(char **s1, char *s2)
+
+static void	ft_strjoinfree(char **s1, char *s2)
 {
 	char	*tmp;
 
@@ -37,7 +38,8 @@ void	ft_strjoinfree(char **s1, char *s2)
 ** Duplicates the string src into dst,
 ** and frees the old dst
 */
-void	ft_strdupfree(char **dst, char *src)
+
+static void	ft_strdupfree(char **dst, char *src)
 {
 	char	*tmp;
 
@@ -49,60 +51,72 @@ void	ft_strdupfree(char **dst, char *src)
 	}
 }
 
-int		get_next_line(int fd, char **line)
+/*
+** Joins the line form previous overflow, to first param
+** then update overflow from endl up to the end of overflow
+*/
+
+static void	save_line(char **line, char **overflow, char *endl)
+{
+	endl[0] = 0;
+	ft_strjoinfree(line, *overflow);
+	ft_strdupfree(overflow, endl + 1);
+}
+
+/*
+** Reads a line and stores it to line arg
+** if there is overflow from previous reading
+** it's taken into consideration by joining it with line
+*/
+
+static int	read_line(int fd, char *buff, char **line, char **overflow)
+{
+	int		flag;
+	char	*endl;
+
+	while ((flag = read(fd, buff, BUFF_SIZE)) > 0 || (flag == 0 && **overflow))
+	{
+		ft_strjoinfree(overflow, buff);
+		if ((endl = ft_strchr(*overflow, '\n')))
+		{
+			save_line(line, overflow, endl);
+			return (1);
+		}
+		else
+		{
+			ft_strjoinfree(line, *overflow);
+			ft_strclr(buff);
+			ft_strclr(*overflow);
+		}
+	}
+	if (!**overflow)
+		ft_strdel(overflow);
+	if (flag == 0 && **line)
+		return (1);
+	return (flag < 0 ? -1 : 0);
+}
+
+int			get_next_line(int fd, char **line)
 {
 	static char		*overflow;
 	char			buff[BUFF_SIZE + 1];
-	int				bytes;
 	char			*endl;
 
-	if (fd < 0 || !line)
+	if (fd < 0 || !line || BUFF_SIZE < 1)
 		return (-1);
 	ft_bzero(buff, BUFF_SIZE + 1);
 	ALLOC_LINE(*line);
 	ft_strclr(*line);
 	if (!overflow)
 		ALLOC_OVERFLOW(overflow);
-	/* Is there overflow from previous reading ? */
-	if (*overflow)
+	if (*overflow && (endl = ft_strchr(overflow, '\n')))
 	{
-		if ((endl = ft_strchr(overflow, '\n')))
-		{
-			/* Duplicate code here: DRY /!\ */
-			endl[0] = 0;
-			ft_strjoinfree(line, overflow); 
-			ft_strdupfree(&overflow, endl + 1); 
-			return (1);
-		}
-	}
-	while ((bytes = read(fd, buff, BUFF_SIZE)) > 0 || (bytes == 0 && *overflow))
-	{
-		ft_strjoinfree(&overflow, buff);	
-		if ((endl = ft_strchr(overflow, '\n')))
-		{
-			/* Duplicate code here: DRY /!\ */
-			endl[0] = 0;
-			ft_strjoinfree(line, overflow); 
-			ft_strdupfree(&overflow, endl + 1); 
-			return (1);
-		}
-		else
-		{
-			/* Join overflow with line */
-			ft_strjoinfree(line, overflow);
-			ft_strclr(buff);
-			ft_strclr(overflow);
-			//if (!bytes)
-			//	return (1);
-		}
-	}
-	if (!*overflow)
-		ft_strdel(&overflow);
-	if (bytes == 0 && **line)
+		save_line(line, &overflow, endl);
 		return (1);
-	return (bytes < 0 ? -1: 0);
+	}
+	return (read_line(fd, buff, line, &overflow));
 }
-
+/*
 int		main(int ac, char **av)
 {
 	int		fd;
@@ -112,13 +126,12 @@ int		main(int ac, char **av)
 	if (ac != 2)
 		exit(1);
 	fd = open(av[1], O_RDONLY);
-	while (get_next_line(fd, &line) > 0)
+	while ((ret = get_next_line(fd, &line)) > 0)
 	{
 		printf(">>[%s]<<\n", line);
 		free(line);
 	}
 	free(line);
-	//printf(">>last[%d]read<<\n", ret);
-	//printf(">>last[%s]read<<\n", line);
 	return (0);
 }
+*/
--- get_next_line.h
+++ get_next_line.h
@@ -6,17 +6,24 @@
 /*   By: akhossan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/06 22:04:56 by akhossan          #+#    #+#             */
-/*   Updated: 2019/04/17 21:32:40 by abkh             ###   ########.fr       */
+/*   Updated: 2019/04/18 21:22:29 by akhossan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE	9999
+# define BUFF_SIZE	999
 # include <unistd.h>
 # include <stdlib.h>
 # include "libft.h"
 
-int		get_next_line(int fd, char **line);
+typedef struct		s_filed
+{
+	int				fd;
+	char			*overflow;
+	struct s_filed	*next;
+}					t_filed;
+
+int					get_next_line(int fd, char **line);
 
 #endif
